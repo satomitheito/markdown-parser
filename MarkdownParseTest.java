076520aa481678dd0dae2dda79192139e5cdd3c7
satomitheito <58501820+satomitheito@users.noreply.github.com>
2022-04-21 17:12:25 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,3 +1,5 @@
+// Imports the assert comparison code 
+//which returns a boolean comparing the left and right inputs
 import static org.junit.Assert.*;
 import org.junit.*;
 public class MarkdownParseTest {
@@ -5,5 +7,10 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    // public void getContent() throws IOException{
+    //     String contet = Files.readString(filePath);
+    //     Path filePath = Path.of(link);
+    // }
 }
 

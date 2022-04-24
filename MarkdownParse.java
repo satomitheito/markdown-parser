1d324ae9e3ac3ef341c2520846da070c7c4a151c
satomitheito <58501820+satomitheito@users.noreply.github.com>
2022-04-23 17:09:06 -0700

    changed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,16 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if (!markdown.substring(currentIndex).contains("[")){
+                break;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-
-
+            
             if(!markdown.substring(closeParen).contains("[")){
                 break;
             }

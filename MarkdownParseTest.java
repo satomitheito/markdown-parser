c70dddd031f7ae1e2edb05c28ea352fcf03ead9d
satomitheito <58501820+satomitheito@users.noreply.github.com>
2022-04-28 08:36:35 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,15 +2,33 @@
 //which returns a boolean comparing the left and right inputs
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MarkdownParseTest {
+    static ArrayList<String> test; 
+
+    @Before 
+    public void setUp() {
+        test = new ArrayList<String>();
+        test.add("https://something.com");
+        test.add ("some-thing.html");
+    }
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
 
-    // public void getContent() throws IOException{
-    //     String contet = Files.readString(filePath);
-    //     Path filePath = Path.of(link);
-    // }
+    @Test
+    public void getLinkstest() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+    }
 }
 
